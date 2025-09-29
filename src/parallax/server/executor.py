ecd40f8028bbbb28bf7553ed99e064fe51218621
TianyiZhao1437 <tim@gradient.network>
2025-09-29 19:07:06 +0800

    fix: mlx model name map & fix decode wrong seq length for hidden states

--- src/parallax/server/executor.py
+++ src/parallax/server/executor.py
@@ -62,6 +62,13 @@ from parallax_utils.utils import compute_max_batch_size
 logger = get_logger(__name__)
 
 
+"""Currently hard code model name for MAC"""
+MLX_MODEL_NAME_MAP = {
+    "openai/gpt-oss-20b": "mlx-community/gpt-oss-20b-MXFP4-Q8",
+    "openai/gpt-oss-120b": "mlx-community/gpt-oss-120b-MXFP4-Q8",
+}
+
+
 class Executor:
     """High-level executor for managing model shards, scheduler, and cache pool on each Peer."""
 
@@ -128,6 +135,9 @@ class Executor:
             self.running_batch = ScheduleBatch(reqs=[], batch_is_full=False)
             self.cur_batch = None
         else:
+            mlx_model_repo = MLX_MODEL_NAME_MAP.get(model_repo, None)
+            if mlx_model_repo is not None:
+                model_repo = mlx_model_repo
             logger.debug(
                 f"Initializing MLX sharded model loader for repo={model_repo}, layers=[{start_layer}, {end_layer})"
             )
@@ -948,12 +958,21 @@ class Executor:
                 hidden_state_for_req = hidden_states[i : i + 1]
             else:
                 # Other peers get a 3D array of hidden states
-                true_length = int(lengths[i])
-                if hidden_states.ndim == 3:
-                    hidden_state_for_req = hidden_states[i, :true_length, :]
+                if src_request.is_prefill:
+                    true_length = int(lengths[i])
+                    if hidden_states.ndim == 3:
+                        hidden_state_for_req = hidden_states[i, :true_length, :]
+                    else:
+                        hidden_state_for_req = hidden_states[
+                            pre_length : pre_length + true_length, :
+                        ]
+                    pre_length += true_length
                 else:
-                    hidden_state_for_req = hidden_states[pre_length : pre_length + true_length, :]
-                pre_length += true_length
+                    if hidden_states.ndim == 3:
+                        hidden_state_for_req = hidden_states[i, :, :]
+                    else:
+                        hidden_state_for_req = hidden_states[pre_length : pre_length + 1, :]
+                    pre_length += 1
 
             next_req = self._prepare_next_single_request(src_request, hidden_state_for_req)
             batched_requests.append(next_req)
