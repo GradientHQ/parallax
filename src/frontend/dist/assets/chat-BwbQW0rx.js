9da126b17bf4195b5c2789433e3bc6db1afa2517
Xiaodong <xz@gradient.network>
2025-10-23 14:41:35 +0800

    build(frontend): build for front-end

--- src/frontend/dist/assets/chat-BwbQW0rx.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,C as o}from"./App-BqBanBeC.js";t.createRoot(document.getElementById("root")).render(e.jsx(o,{}));
--- src/frontend/dist/assets/main-DVt474jQ.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,M as o}from"./App-BqBanBeC.js";t.createRoot(document.getElementById("root")).render(e.jsx(o,{}));
