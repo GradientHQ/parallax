ea9a79933efbc61e6ac8c11fcc377cef656ea4d6
Xiaodong <xz@gradient.network>
2025-11-19 15:45:53 +0800

    feat(frontend): supports display gpu number in node

---
+++ src/frontend/dist/assets/chat-B9EnTaDS.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,C as o}from"./App-CWt1GYdV.js";t.createRoot(document.getElementById("root")).render(e.jsx(o,{}));
---
+++ src/frontend/dist/assets/main-D0zKlwlx.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,M as o}from"./App-CWt1GYdV.js";t.createRoot(document.getElementById("root")).render(e.jsx(o,{}));
