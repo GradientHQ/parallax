4c9c022e6af806583c6e38adbe05448581dd07c2
TianyiZhao1437 <tim@gradient.network>
2025-12-11 19:38:45 +0800

    update

--- src/parallax/metal/extensions/setup.py
+++ src/parallax/metal/extensions/setup.py
@@ -1,18 +1,17 @@
 # Copyright © 2023-2024 Apple Inc.
 
-from setuptools import setup
-
 from mlx import extension
+from setuptools import setup
 
 if __name__ == "__main__":
     setup(
-        name="ops",
-        version="0.0.0",
+        name="parallax_ops",
+        version="0.0.1",
         description="Metal op extensions.",
-        ext_modules=[extension.CMakeExtension("ops._ext")],
+        ext_modules=[extension.CMakeExtension("parallax_ops._ext")],
         cmdclass={"build_ext": extension.CMakeBuild},
-        packages=["ops"],
-        package_data={"ops": ["*.so", "*.dylib", "*.metallib"]},
+        packages=["parallax_ops"],
+        package_data={"parallax_ops": ["*.so", "*.dylib", "*.metallib"]},
         zip_safe=False,
         python_requires=">=3.10",
     )
