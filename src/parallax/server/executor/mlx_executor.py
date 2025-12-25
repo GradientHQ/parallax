3dc6e8abe20ebb51d8f002110e6b3510f6766158
TianyiZhao1437 <tim@gradient.network>
2025-12-25 14:17:48 +0000

    refactor weight concat subprocess to engine

--- src/parallax/server/executor/mlx_executor.py
+++ src/parallax/server/executor/mlx_executor.py
@@ -2,6 +2,7 @@
 MLX-LM backend implementation of high level executor
 """
 
+import threading
 import time
 from typing import Any, Dict, List, Optional, Tuple
 
@@ -18,11 +19,13 @@ from parallax.server.request import (
 from parallax.server.sampling.sampler import SamplingBatchInfo
 from parallax.server.shard_loader import MLXModelLoader
 from parallax.utils.utils import (
+    WeightRefitStatus,
     combine_padding_and_causal_masks,
     create_causal_mask,
     get_device_dtype,
     get_layer_types,
     pad_inputs,
+    concat_weight_partition,
 )
 from parallax_utils.logging_config import get_logger
 
@@ -316,8 +319,26 @@ class MLXExecutor(BaseExecutor):
 
     def check_and_refit_weight(self, refit_weight_path: str):
         if refit_weight_path == "":
-            return
-        self.shard_loader.update_weight_from_disk(self.model_shard, refit_weight_path)
+            if self.weight_refit_status == WeightRefitStatus.READY_FOR_REFIT:
+                self.shard_loader.update_weight_from_disk(self.model_shard, self.saved_weight_path)
+                self.weight_refit_status = WeightRefitStatus.FINISH_REFIT
+            else:
+                return
+        else:
+            if self.weight_refit_status == WeightRefitStatus.FINISH_REFIT:
+                self.weight_refit_status = WeightRefitStatus.CONCAT
+                self.saved_weight_path = refit_weight_path
+                # launch a subthread to concat weight files and save to disk
+                t = threading.Thread(
+                        target=concat_weight_partition,
+                        args=(refit_weight_path),
+                        daemon=True,
+                    )
+                t.start()
+            else:
+                raise ValueError(
+                    f"Receive refit request while not finish previous refit process. "
+                )
 
     def process_batch(self, prepared_inputs: Dict[str, Any], return_decoded_tokens: bool = True):
         """Process a batch of requests in MLX."""
