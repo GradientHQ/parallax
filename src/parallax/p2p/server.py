48637d0b9b6ed4876b6bf3ea5a08c9b20fabcfaa
TianyiZhao1437 <tim@gradient.network>
2025-12-31 15:48:59 +0800

    update

--- src/parallax/p2p/server.py
+++ src/parallax/p2p/server.py
@@ -32,7 +32,6 @@ from parallax.utils.utils import get_zmq_socket
 from parallax.utils.weight_refit_utils import (
     calculate_cid_manual,
     concat_weight_partition,
-    filer_weight_cid_list,
     release_disk_storage,
 )
 from parallax_utils.logging_config import get_logger, set_log_level
@@ -263,20 +262,14 @@ def check_and_run_weight_refit(gradient_server, message):
 
     # step1. Check weight refit trigger message
     time_stamp = message.get("time_stamp", None)
-    index_map = message.get("index_map", None)
+    cid_list = message.get("cid", None)
     weight_version = message.get("version", 0)
-    if time_stamp is None or index_map is None:
+    if time_stamp is None or cid_list is None:
         return
     if gradient_server.last_refit_time >= float(time_stamp):
         # Weight already updated
         return
 
-    cid_list = filer_weight_cid_list(
-        gradient_server.block_start_index,
-        gradient_server.block_end_index,
-        gradient_server.hidden_layers,
-        index_map,
-    )
     random.seed(time.time())
     random.shuffle(cid_list)
 
