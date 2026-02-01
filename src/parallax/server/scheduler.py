dd45122edd9aedcfa90a19a51207be8e4d36c58e
wasamtc <wasam@qq.com>
2026-02-01 02:08:17 +0000

    fix[chunked-prefill]: add logs

--- src/parallax/server/scheduler.py
+++ src/parallax/server/scheduler.py
@@ -239,6 +239,7 @@ class Scheduler:
             req = self._wait_queue.popleft()
             rid = req.request_id
             if rid in self._running_requests:
+                logger.debug(f"Request {rid} already in running requests. skip admit.")
                 continue
 
             # Check kv cache pool
